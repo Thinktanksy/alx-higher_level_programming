e9a4e5c6eb5b6e78aa64543638fd85c71fcde279
Joseph Williams L <richlike.joseph@gmail.com>
2023-03-09 01:51:12 -0800

    Python Import and Moules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
 
-    total = 0
+        print("{:d}".format(n))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(argv[i])
+            1 += 1
+            print("{:d}".format(add))
 
-    for arg in args:
-        total += int(arg)
-        print(total)
+
+            if __name__ == "__main__":
+                import sys
+                add_arg(sys.argv)
