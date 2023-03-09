434196f856e54050132114328d1c29a08800cd3a
Joseph Williams L <richlike.joseph@gmail.com>
2023-03-09 01:18:42 -0800

    Python Import and Moules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,17 +2,12 @@
 
 
 if __name__ == "__main__":
-    """Print the sum, difference, multiple and quotient of 10 and 5."""
-
     from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
 
-    sum_result = add(a,b)
-    diff_result = subtract(a,b)
-    prod_result = multiply(a,b)
-    quot_result = divide(a,b)
-
-    print("The sum of {} and {} is {}".format(a, b, sum_result))
-    print("The difference of {} and {} is {}".format(a, b, diff_result))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
