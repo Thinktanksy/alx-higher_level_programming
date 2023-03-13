8be422ace376150ef76b934e46e3225a2d13635f
Joseph Williams L <richlike.joseph@gmail.com>
2023-03-12 23:40:17 -0700

    Python Data structure Tulips

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
-# 7-add_tuple.py
-
-def add_tuple(tuple_a=(), tuple_b=()):
-    """Add two tuples."""
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = 0, 0
-        else:
-            tuple_a = tuple_a[0], 0
-            if len(tuple_b) < 2:
-                if len(tuple_b) == 0:
-                    tuple_b = 0, 0
-                else:
-                    tuple_b = tuple_b[0], 0
-
-                    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else "None"
+    tup = length, first_char
+    return(tup)
