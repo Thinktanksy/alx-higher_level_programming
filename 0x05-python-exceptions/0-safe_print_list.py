bb3393443c2d5796ab3bd41c369b0f4a8ced28fa
Joseph Williams L <richlike.joseph@gmail.com>
2023-03-27 05:10:25 -0700

    Python Exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    r_num = 0
-    for i in my_list:
-        for i in range(x):
-            try:
-                print(my_list[i], end="")
-                r_num += 1
+    total = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            total += 1
+        except IndexError:
+            break
+        print("")
+        return (total)
