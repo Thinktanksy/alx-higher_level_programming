1dc05f85d23e8c70775223a78d8d88017f99327c
Joseph Williams L <richlike.joseph@gmail.com>
2023-04-04 03:20:19 -0700

    Python Everything is Object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" 101-locked_class """
+
+class LockedClass:
+        """ locked class """
+            #  define available slots that can be created
+                __slots__ = ('first_name')
