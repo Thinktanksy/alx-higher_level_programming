f43857904a137e9bd601a03c328e4da7462f428c
Joseph Williams L <richlike.joseph@gmail.com>
2023-04-11 04:57:16 -0700

    Python Inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,5 +6,6 @@ class BaseGeometry:
     """A class to represent a base geometry"""
 
     def area(self):
-    """ Method to raise an exception when an area of the geometry is not defined."""
+    """ Method to raise an exception when an area
+    of the geometry is not defined."""
     raise Exception("area() is not implemented")
