bb3f7c4e4eb084e4896839d8d19153621ea42c63
Joseph Williams L <richlike.joseph@gmail.com>
2023-04-11 05:26:47 -0700

    Python Inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,10 +2,12 @@
 """this module defines a class MyInt that inherits from int"""
 
 class MyInt(int):
-    # Overrides == operator
-    def __eq__(self, other):
-        return not super().__eq__(other)
-        
-    # Overrides != operator
-    def __ne__(self, other):
-        return not super().__ne__(other)
+    """Invert int operators == and !="""
+
+    def __eq__(self, value):
+        """Override == opeartor with != behavior"""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior"""
+        return self.real == value
